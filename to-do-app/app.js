831eda09595801e067d579cf034de41744e8b6cd
Seira <seiray117@gmail.com>
2019-03-11 12:10:45 -0700

    checkpoint_DOMScripting2

---
+++ to-do-app/app.js
@@ -0,0 +1,59 @@
+function onReady(){
+ const addToDoForm = document.getElementById('addToDoForm');
+ let toDos = [];
+ let id = 0;
+
+ function createNewToDo(){
+   const newToDoText = document.getElementById('newToDoText');
+   if(!newToDoText.value){ return; }
+   toDos.push({
+     title: newToDoText.value,
+     complete: false,
+     id: id
+   });
+
+  id++;
+
+   newToDoText.value = '';
+   renderTheUI();
+ }
+
+function renderTheUI(){
+  const toDoList = document.getElementById('toDoList');
+ //when I render the UI, donot put the exsiting Li into UI
+  toDoList.textContent = '';
+
+  toDos.forEach(function(toDo) {
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener('click', event => {
+      toDos = toDos.filter(function(item){
+        return item.id !== toDo.id;
+      })
+
+   renderTheUI();
+  });
+
+    newLi.textContent = toDo.title;
+    newLi.appendChild(checkbox);
+    toDoList.appendChild(newLi);
+    newLi.appendChild(deleteButton);
+  })
+}
+
+ addToDoForm.addEventListener('submit', event => {
+   event.preventDefault();
+   createNewToDo();
+ });
+
+ renderTheUI();
+}
+
+window.onload = function(){
+  onReady();
+};
